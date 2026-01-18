3490808cda77f65de2ebf274afa679c34f487ff0
Ishita Gupta <112006649+Ishita1604@users.noreply.github.com>
2026-01-18 22:40:36 +0530

    SLL - Reverse Singly Linked List
    Turn the tables around & undo the links! ✿

---
+++ 01-Linked List/00-Basic/06_Reverse_LL.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Link: https://leetcode.com/problems/reverse-linked-list/
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        // TIME:  O(N)
+        // SPACE: O(1)
+        // Initialise ---
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* nxt = NULL;
+        
+        while (curr != NULL) {
+            // Preserve address of next LL.
+            nxt = curr->next;
+            // Reversal ---
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+
+        // New head is prev. Also curr & next both become NULL at end.
+        return prev;
+    }
+};
